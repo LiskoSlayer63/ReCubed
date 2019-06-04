0fb2a933c2aa300e70bfae2a60396b502431f279
LiskoSlayer63 <markus.ahola@hotmail.com>
2019-06-04 08:14:07 +0300

    Corrected build number
    Run configuration doesn't work properly so changed LibMisc to manual build number.

--- src/main/java/vazkii/recubed/common/lib/LibMisc.java
+++ src/main/java/vazkii/recubed/common/lib/LibMisc.java
@@ -14,7 +14,7 @@ public final class LibMisc {
 
 	public static final String MOD_ID = "recubed";
 	public static final String MOD_NAME = "ReCubed";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.2-8";
 
 	public static final String COMMON_PROXY = "vazkii.recubed.common.core.proxy.CommonProxy";
 	public static final String CLIENT_PROXY = "vazkii.recubed.client.core.proxy.ClientProxy";
